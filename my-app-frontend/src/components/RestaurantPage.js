b9b788f65e7bc7f49f933653af24b1314016124b
Hunter Kassow <hunterkassow@protonmail.com>
2022-07-11 13:14:40 -0400

    add fetch/state for intial restaurants render

--- my-app-frontend/src/components/RestaurantPage.js
+++ my-app-frontend/src/components/RestaurantPage.js
@@ -1,6 +1,14 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 function RestaurantPage ()  {
+    // const [restaurantList, setRestaurantList] = useState([])
+
+    // useEffect(() => {
+    //     fetch("http://localhost:9292/restaurants?include_first_review")
+    //     .then(r => r.json)
+    //     .then(d => setRestaurantList(d))
+        
+    // },[])
     return (<h1> hi</h1>)
 }
 
