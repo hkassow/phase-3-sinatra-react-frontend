e4e6f1689f071aa9ecdae9c9f2cc566f7a964c66
Hunter Kassow <hunterkassow@protonmail.com>
2022-07-12 00:14:12 -0400

    readability changes

--- my-app-frontend/src/components/helper.js
+++ my-app-frontend/src/components/helper.js
@@ -9,10 +9,10 @@ prettyTime = () => {
 //review date makes pretty then returns diff from today and that date
 daysSince = (review_date) => {
     //new Date takes in YEAR MONTH from DAY 
-    let date = prettyTime(review_date)
-    const oneDay = 24 * 60 * 60 * 1000;
-    date = new Date(date[0], date[1] - 1, date[2])
     let today = new Date(new Date().toLocaleString())
+    let date = prettyTime(review_date)
+    date = new Date(date[0], date[1] - 1, date[2])
+    const oneDay = 24 * 60 * 60 * 1000;
     const diffDays = Math.round(Math.abs((today - date) / oneDay));
     return diffDays
 }
