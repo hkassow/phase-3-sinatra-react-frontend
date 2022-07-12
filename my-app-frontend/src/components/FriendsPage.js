dc5017506077a02c96cb15fa659ff026058cac60
Hunter Kassow <hunterkassow@protonmail.com>
2022-07-11 21:02:19 -0400

    helper functions, days since review date

--- my-app-frontend/src/components/FriendsPage.js
+++ my-app-frontend/src/components/FriendsPage.js
@@ -1,5 +1,10 @@
-import React from "react"
+import React, { useEffect } from "react"
 function FriendsPage () {
+    useEffect(() => {
+        fetch("http://localhost:9292/user/:id?friends")
+        .then(r => r.json())
+        .then(d => console.log(d))
+    })
     return <></>
 }
 export default FriendsPage
