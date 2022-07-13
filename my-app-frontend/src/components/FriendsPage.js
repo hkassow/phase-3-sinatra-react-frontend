f1ee74021151026539a0b78357f9ffb906e6417b
Hunter Kassow <hunterkassow@protonmail.com>
2022-07-13 14:14:59 -0400

    friends initial layout

--- my-app-frontend/src/components/FriendsPage.js
+++ my-app-frontend/src/components/FriendsPage.js
@@ -4,34 +4,43 @@ import styled from "styled-components";
 function FriendsPage() {
   return (
     <StyledFriendsPage>
-      <Heading>Friends</Heading>
+        <Heading>Friends</Heading>
+        <Left>
+          <h1>Show friend reviews</h1>
+          <ul> 
+            <h1>Show:</h1>
+            <li>friend1</li>
+            <li>friend2</li>
+          </ul>
+        </Left>
+        <Center>Center</Center>
     </StyledFriendsPage>
   );
 }
 
 const StyledFriendsPage = styled.div`
   min-height: 100vh;
-  width: 100vw;
+  grid-template-columns: max-content;
   background-color: #282c34;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  display: grid;
+  grid-gap: 10px;
+  padding: 10px;
 `;
-
-const Heading = styled.h1`
-  font-size: clamp(3rem, 5vw, 7vw);
-  color: #eee;
-  font-weight: 700;
-  margin: 0;
-  padding: 0;
-
-  user-select: none;
-  -webkit-user-select: none;
-  -khtml-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-`;
+const Center = styled.div`
+  text-align center;
+  grid-area: 2 / 2 / auto / span 2
+`
+const Left = styled.div`
+  grid-area: 2 / 1 / auto / span 1;
+  text-align center;
+  max-width: 200px;
+`
+const Heading = styled.div`
+  grid-area: 1 / 1 /1 / span 3;
+  background-color: rgba(255, 255, 255, 0.8);
+  text-align: center;
+  padding: 20px 0;
+  font-size: 30px;
+`
 
 export default FriendsPage;
